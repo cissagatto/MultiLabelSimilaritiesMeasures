8104d0eb0ecf7a54883d48cc191c03fb1f317edd
Elaine Cecília Gatto <elainececiliagatto@gmail.com>
2021-10-19 16:38:39 -0300

    Add files via upload

--- R/run.R
+++ R/run.R
@@ -113,80 +113,230 @@ res3
 funs = getNamesListFunctions()
 funs
 
+# retornou números positivos e infinitos
 compute.measure(labels, num.labels, a, b, c, d, n, funs[1], ample.e)
+
+# retornou números negativos e positivos
 compute.measure(labels, num.labels, a, b, c, d, n, funs[2], anderberg.e)
+
+# retornou valores positivos entre 0 e 1
 compute.measure(labels, num.labels, a, b, c, d, n, funs[3], baroni.urbani.buser.1.e)
+
+# retornou valores positivos e negativos. 
+# A faixa deve estar entre -1 e +1
 compute.measure(labels, num.labels, a, b, c, d, n, funs[4], baroni.urbani.buser.2.e)
+
+# retornou valores positivos entre 0 e 1
 compute.measure(labels, num.labels, a, b, c, d, n, funs[5], braun.banquet.e)
+
+# retornou valores positivos entre 0 e 1
 compute.measure(labels, num.labels, a, b, c, d, n, funs[6], bray.curtis.e)
+
+# retornou valores positivos dentro dos valores da tabela de contingência
 compute.measure(labels, num.labels, a, b, c, d, n, funs[7], canberra.e)
+
+# retornou valores positivos 
 compute.measure(labels, num.labels, a, b, c, d, n, funs[8], chord.e )
+
+# retornou valores positivos dentro dos valores da tabela de contingência
 compute.measure(labels, num.labels, a, b, c, d, n, funs[9], cityblock.e)
+
+# retornou apenas INFs and NANs
 compute.measure(labels, num.labels, a, b, c, d, n, funs[10], cole.e)
+
+# retornou valores positivos
 compute.measure(labels, num.labels, a, b, c, d, n, funs[11], cosine.e)
+
+# retornou valores positivos entre 0 e 1
 compute.measure(labels, num.labels, a, b, c, d, n, funs[12], czekanowski.e)
+
+# retornou valores positivos e negativos
 compute.measure(labels, num.labels, a, b, c, d, n, funs[13], dennis.e)
+
+# retornou valores positivos entre 0 e 1
 compute.measure(labels, num.labels, a, b, c, d, n, funs[14], dice.e)
+
+# retornou valores positivos e negativos
 compute.measure(labels, num.labels, a, b, c, d, n, funs[15], disperson.e)
+
+# retornou valores positivos entre 0 e 1
 compute.measure(labels, num.labels, a, b, c, d, n, funs[16], driver.kroeber.e)
+
+# retornou valores positivos
 compute.measure(labels, num.labels, a, b, c, d, n, funs[17], euclidean.e)
+
+# retornou valores positivos e negativos
 compute.measure(labels, num.labels, a, b, c, d, n, funs[18], eyraud.e)
+
+# retornou valores positivos e negativos
 compute.measure(labels, num.labels, a, b, c, d, n, funs[19], fager.mcgowan.e)
+
+# retornou valores positivos
 compute.measure(labels, num.labels, a, b, c, d, n, funs[20], faith.e)
-compute.measure(labels, num.labels, a, b, c, d, n, funs[21], forbes.2.e )
+
+# retornou valores positivos
+compute.measure(labels, num.labels, a, b, c, d, n, funs[21], forbes.2.e)
+
+# retornou valores positivos
 compute.measure(labels, num.labels, a, b, c, d, n, funs[22], forbesi.e)
+
+# retornou valores positivos
 compute.measure(labels, num.labels, a, b, c, d, n, funs[23], fossum.e)
+
+# retornou valores positivos, negativos e INFs
 compute.measure(labels, num.labels, a, b, c, d, n, funs[24], gilbert.well.e)
+
+# retornou valores positivos e negativos
 compute.measure(labels, num.labels, a, b, c, d, n, funs[25], goodman.kruskal.e)
+
+# retornou valores positivos
 compute.measure(labels, num.labels, a, b, c, d, n, funs[26], gower.e)
+
+# retornou valores positivos entre 0 e 1
 compute.measure(labels, num.labels, a, b, c, d, n, funs[27], gower.legendre.e)
+
+# retornou valores positivos e negativos
 compute.measure(labels, num.labels, a, b, c, d, n, funs[28], hamann.e)
+
+# retornou valores positivos dentro dos valores da tabela de contingência
 compute.measure(labels, num.labels, a, b, c, d, n, funs[29], hamming.e)
+
+# retornou valores positivos
 compute.measure(labels, num.labels, a, b, c, d, n, funs[30], hellinger.e)
+
+# retornou valores positivos dentro dos valores da tabela de contingência
 compute.measure(labels, num.labels, a, b, c, d, n, funs[31], inner.product.e)
+
+# retornou valores positivos dentro dos valores da tabela de contingência
 compute.measure(labels, num.labels, a, b, c, d, n, funs[32], intersection.e)
+
+# retornou valores positivos entre o e 1
 compute.measure(labels, num.labels, a, b, c, d, n, funs[33], jaccard.e)
+
+# retornou valores positivos
 compute.measure(labels, num.labels, a, b, c, d, n, funs[34], johnson.e)
+
+# retornou valores positivos e INFs
 compute.measure(labels, num.labels, a, b, c, d, n, funs[35], kulczynski.1.e)
+
+# retornou valores positivos
 compute.measure(labels, num.labels, a, b, c, d, n, funs[36], kulczynski.2.e)
+
+# retornou valores positivos entre 0 e 1
 compute.measure(labels, num.labels, a, b, c, d, n, funs[37], lance.williams.e)
+
+# retornou valores positivos dentro dos valores da tabela de contingência
 compute.measure(labels, num.labels, a, b, c, d, n, funs[38], manhattan.e)
+
+# retornou valores positivos e negativos
 compute.measure(labels, num.labels, a, b, c, d, n, funs[39], mcconnaughey.e)
+
+# retornou valores positivos
 compute.measure(labels, num.labels, a, b, c, d, n, funs[40], mean.manhattan.e)
+
+# retornou valores positivos
 compute.measure(labels, num.labels, a, b, c, d, n, funs[41], michael.e)
+
+# retornou valores positivos dentro dos valores da tabela de contingência
 compute.measure(labels, num.labels, a, b, c, d, n, funs[42], minowski.e)
+
+# retornou valores positivos e INFs
 compute.measure(labels, num.labels, a, b, c, d, n, funs[43], mountford.e)
+
+# retornou valores positivos entre 0 e 1
 compute.measure(labels, num.labels, a, b, c, d, n, funs[44], nei.li.e)
+
+# retornou valores positivos entre 0 e 1
 compute.measure(labels, num.labels, a, b, c, d, n, funs[45], ochiai.2.e)
+
+# retornou valores positivos entre 0 e 1
 compute.measure(labels, num.labels, a, b, c, d, n, funs[46], otsuka.e)
+
+# retornou valores positivos
 compute.measure(labels, num.labels, a, b, c, d, n, funs[47], pattern.difference.e)
+
+# retornou valores positivos
 compute.measure(labels, num.labels, a, b, c, d, n, funs[48], pearson.1.e)
+
+# retornou valores positivos
 compute.measure(labels, num.labels, a, b, c, d, n, funs[49], pearson.2.e)
+
+# retornou valores positivos e NANs
 compute.measure(labels, num.labels, a, b, c, d, n, funs[50], pearson.3.e)
+
+# retornou valores positivos e negativos
 compute.measure(labels, num.labels, a, b, c, d, n, funs[51], pearson.heron.1.e)
+
+# retornou valores positivos, negativos e NANs
 compute.measure(labels, num.labels, a, b, c, d, n, funs[52], pearson.heron.2.e)
+
+# retornou valores positivos e NANs
 compute.measure(labels, num.labels, a, b, c, d, n, funs[53], peirce.e)
+
+# retornou valores positivos
 compute.measure(labels, num.labels, a, b, c, d, n, funs[54], roger.tanimoto.e)
+
+# retornou valores positivos
 compute.measure(labels, num.labels, a, b, c, d, n, funs[55], russel.rao.e)
+
+# retornou valores positivos
 compute.measure(labels, num.labels, a, b, c, d, n, funs[56], shape.differnece.e)
+
+# retornou valores positivos entre 0 e 1
 compute.measure(labels, num.labels, a, b, c, d, n, funs[57], simpson.e)
+
+# retornou valores positivos
 compute.measure(labels, num.labels, a, b, c, d, n, funs[58], size.difference.e)
+
+# retornou valores positivos entre 0 e 1
 compute.measure(labels, num.labels, a, b, c, d, n, funs[59], sokal.michener.e)
+
+# retornou valores positivos entre 0 e 1
 compute.measure(labels, num.labels, a, b, c, d, n, funs[60], sokal.sneath.1.e)
+
+# retornou valores positivos entre 0 e 1
 compute.measure(labels, num.labels, a, b, c, d, n, funs[61], sokal.sneath.2.e)
+
+# retornou valores positivos e INFs
 compute.measure(labels, num.labels, a, b, c, d, n, funs[62], sokal.sneath.3.e)
+
+# retornou valores positivos entre 0 e 1
 compute.measure(labels, num.labels, a, b, c, d, n, funs[63], sokal.sneath.4.e)
+
+# retornou valores positivos
 compute.measure(labels, num.labels, a, b, c, d, n, funs[64], sokal.sneath.5.e)
+
+# retornou valores positivos
 compute.measure(labels, num.labels, a, b, c, d, n, funs[65], sorgenfrei.e)
+
+# retornou valores positivos dentro dos valores da tabela de contingência
 compute.measure(labels, num.labels, a, b, c, d, n, funs[66], square.euclidean.e)
+
+# retornou valores positivos
 compute.measure(labels, num.labels, a, b, c, d, n, funs[67], stiles.e)
+
+# retornou valores positivos entre 0 e 1
 compute.measure(labels, num.labels, a, b, c, d, n, funs[68], tanimoto.e)
+
+# retornou valores positivos e INFs
 compute.measure(labels, num.labels, a, b, c, d, n, funs[69], tarantula.e)
+
+# retornou valores positivos, negativos e INFs
 compute.measure(labels, num.labels, a, b, c, d, n, funs[70], tarwid.e)
+
+# retornou valores positivos entre 0 e 1
 compute.measure(labels, num.labels, a, b, c, d, n, funs[71], three.w.jaccard.e)
+
+# retornou valores positivos
 compute.measure(labels, num.labels, a, b, c, d, n, funs[72], vari.e)
+
+# retornou valores positivos, negativos e INFs
 compute.measure(labels, num.labels, a, b, c, d, n, funs[73], yule.w.e)
+
+# retornou valores positivos e negativos entre -1 e +1
 compute.measure(labels, num.labels, a, b, c, d, n, funs[74], yuleq.1.e)
+
+# retornou valores positivos e INFs
 compute.measure(labels, num.labels, a, b, c, d, n, funs[75], yuleq.2.e)
 
 
